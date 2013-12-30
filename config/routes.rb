8ffb8e38eb4d1b69082274d1cdbdd0dcc6644e1c
Mahm0ud <mahmoud.reda@nileu.edu.eg>
2013-12-30 22:31:02 +0200

    Adding the route to the balance sheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Ostazonline::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  get "main/balance_sheet"
   resources :accounts
   
   resources :transactions
