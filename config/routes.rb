f7078f499ddcd4b6848b1786fcdf5c0938c19315
Mahm0ud <mahmoud.reda@nileu.edu.eg>
2013-12-30 22:09:45 +0200

    Change routes index to refer directly to main root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Ostazonline::Application.routes.draw do
-  get "main/index"
-
-  get "main/balance_sheet"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
   resources :accounts
@@ -54,7 +50,7 @@ Ostazonline::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
