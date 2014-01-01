15a133d11520e396ced4628039fccf64fd33c3ee
Mahm0ud <mahmoud.reda@nileu.edu.eg>
2014-01-02 00:51:36 +0200

    Adding db files for trouble shooting multi env issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131216204652) do
+ActiveRecord::Schema.define(:version => 20140101191111) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -31,8 +31,12 @@ ActiveRecord::Schema.define(:version => 20131216204652) do
 
   create_table "transactions", :force => true do |t|
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
   end
 
 end
