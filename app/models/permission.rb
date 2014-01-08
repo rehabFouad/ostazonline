5168e882ed14f5ac0699b1bc3a715415c78f9ea9
Mahm0ud <mahmoud.reda@nileu.edu.eg>
2014-01-09 00:52:17 +0200

    Chaning Authorization model to be subject/action based instead of role based

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,3 +1,4 @@
 class Permission < ActiveRecord::Base
+  belongs_to :user
   attr_accessible :action, :description, :name, :subject_class, :subject_id, :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,31 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :permissions
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :role, :permissions
-  validates_presence_of :role
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
+  has_many :permissions, :dependent => :destroy
+  accepts_nested_attributes_for :permissions, :allow_destroy => true
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :role, :permissions ,:permissions_attributes
   before_create :setup_username_from_email
-
-
+  before_create :setup_default_role_for_new_users
   def self.rols
     [:Observer,:Accountant,:Entry]
   end
-  
+
+  def self.actions
+    [:manage, :create, :read, :update, :destroy]
+  end
+
+  def self.subjects
+    ["Account","Transaction","User","all"]
+  end
+
   private
+
   def setup_username_from_email
-      self.username = self.email.split('@')[0]
+    self.username = self.email.split('@')[0]
+  end
+
+  def setup_default_role_for_new_users
+    if self.role.blank?
+      self.role = :Default
+    end
   end
 end
